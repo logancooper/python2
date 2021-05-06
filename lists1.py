3d7b4a07bf6e165662000838c3c564f4a348075f
Logan Cooper <logancooper@bellsouth.net>
2021-05-06 14:16:43 -0400

    list exercises

---
+++ lists1.py
@@ -0,0 +1,8 @@
+mylist = ["World of Warcraft", "Bioshock", "Portal", "Zelda"]
+
+print(len(mylist))
+
+index = 0
+while(index < len(mylist)):
+    print(mylist[index])
+    index += 1
---
+++ loop1.py
@@ -0,0 +1,12 @@
+title = "my title"
+COUNTER_MAX = len(title)
+counter = 0
+
+while(counter < COUNTER_MAX):
+    print(title[counter])
+    counter = counter + 1
+
+
+
+#for(c = 0; c < COUNTER_MAX; c += 1)
+
